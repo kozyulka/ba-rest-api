612d438e3efab1d0cc9491c634ec76af2db6b559
Daryna Gavrylenko <deakozyulka@gmail.com>
2018-07-14 14:54:25 +0300

    remove function

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -1,7 +1,7 @@
 const user = require("./user");
 const message = require("./message");
 
-module.exports = function(app) {
+module.exports = (app) => {
     app.use("/api/users", user);
     app.use("/api/messages", message);
 };
